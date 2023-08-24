0dc23225c16cfc415118710be840fc3983de077c
Leo Chong <leochong0108@gmail.com>
2023-08-24 14:56:52 +0800

    updated

--- src/Bank/Account/SavingsAccount.java
+++ src/Bank/Account/SavingsAccount.java
@@ -1,5 +1,11 @@
 package Bank.Account;
 
+import static Bank.Account.Account.DB_URL;
+import static Bank.Account.Account.DB_USER;
+
+
+import java.sql.*;
+
 public class SavingsAccount extends Account{
     
     private double interestRate = 0.6;
@@ -24,6 +30,29 @@ public class SavingsAccount extends Account{
 
     public void setBalance(double balance){
         this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+
+        balance += amount;
+
+        try(Connection connection = getConnection()) {
+
+            Statement statement = connection.createStatement();
+
+            String updateStatement = "UPDATE users SET balance=" + balance + " WHERE id=1";
+            
+            PreparedStatement preparedStatement = connection.prepareStatement(updateStatement);
+
+            preparedStatement.executeUpdate();
+
+            statement.close();
+            connection.close();
+            
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
